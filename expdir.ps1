af426413f03420c3ac7f01f587982960e6bb4508
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-11-10 14:30:48 +0100

    added help

--- expdir.ps1
+++ expdir.ps1
@@ -1,16 +1,27 @@
-cd $PSScriptRoot
-if ($args[0] -eq $null)
+if ($args[0] -eq "--help")
 {
-	$location = Get-Location
+	echo "Usage :"
+	echo ""
+	echo "    expdir [<directory>]"
+	echo ""
+	echo "<directory> : folder to start exploring. By default, the current folder."
 }
 else
 {
-	$location = $args[0]
-}
-./expdirapp.exe $location
-if(Test-Path script.ps1)
-{
-	$curr = Get-Location
-	./script.ps1
-	rm "$curr/script.ps1"
+	cd $PSScriptRoot
+	if ($args[0] -eq $null)
+	{
+		$location = Get-Location
+	}
+	else
+	{
+		$location = $args[0]
+	}
+	./expdirapp.exe $location
+	if(Test-Path script.ps1)
+	{
+		$curr = Get-Location
+		./script.ps1
+		rm "$curr/script.ps1"
+	}
 }
