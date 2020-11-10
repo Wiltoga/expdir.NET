21bb3f28e964770d44fc1149a8a5a073ae4df43a
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-11-10 14:23:20 +0100

    proper Windows support

--- expdir.ps1
+++ expdir.ps1
@@ -1,7 +1,16 @@
-./expdirapp.exe
+cd $PSScriptRoot
+if ($args[0] -eq $null)
+{
+	$location = Get-Location
+}
+else
+{
+	$location = $args[0]
+}
+./expdirapp.exe $location
 if(Test-Path script.ps1)
 {
 	$curr = Get-Location
 	./script.ps1
-	rm "$($curr)/script.ps1"
+	rm "$curr/script.ps1"
 }
